f3e9124481bf497314d4f625472d3ea97bfd5783
WiredAndTired <hasinisraque.robo@gmail.com>
2025-03-13 14:05:13 +0600

    speed setup added

--- src/Code/wro25.ino
+++ src/Code/wro25.ino
@@ -16,14 +16,17 @@ const int enterButton = 35;
 // Debounce variables
 unsigned long lastButtonPressTime = 0;
 unsigned long debounceDelay = 200; // debounce delay in milliseconds
-unsigned long downButtonPressTime = 0; // Track down button press duration
-unsigned long downButtonHoldDuration = 2000; // 2 seconds to exit submenu
+unsigned long buttonPressTime = 0; // Track both buttons press duration
+unsigned long buttonsHoldDuration = 2000; // 2 seconds to exit submenu when both buttons are held
 
 bool inSubMenu = false; // Flag to track if we are in submenu
 
 // EEPROM constants
 const int eepromStateAddress = 0; // EEPROM address to store menu state (0 = main menu, 1 = submenu)
 const int eepromSelectedItemAddress = 1; // EEPROM address to store selected menu item
+const int eepromSpeedAddress = 2; // EEPROM address to store speed value
+
+int speed = 0; // Initial speed value
 
 void setup() {
   tft.init();
@@ -43,6 +46,9 @@ void setup() {
   // Read the selected item from EEPROM
   selectedItem = EEPROM.read(eepromSelectedItemAddress);
 
+  // Read the speed from EEPROM
+  speed = EEPROM.read(eepromSpeedAddress);
+
   // If in submenu, start from submenu state
   if (inSubMenu) {
     enterSubMenu(); // Enter the submenu directly
@@ -53,7 +59,7 @@ void setup() {
 
 void loop() {
   unsigned long currentMillis = millis();
-  
+
   // Enter button handling (Enter submenu)
   if (digitalRead(enterButton) == LOW && currentMillis - lastButtonPressTime > debounceDelay) {
     if (!inSubMenu) {
@@ -62,24 +68,46 @@ void loop() {
     lastButtonPressTime = currentMillis; // Update the last button press time
   }
 
-  // Down button handling (Hold to exit submenu)
+  // Both buttons pressed and held for 2 seconds to exit submenu
+  if (digitalRead(downButton) == LOW && digitalRead(enterButton) == LOW) {
+    if (buttonPressTime == 0) {
+      buttonPressTime = currentMillis; // Start tracking the hold time
+    }
+    // If both buttons are held for 2 seconds, exit submenu
+    else if (currentMillis - buttonPressTime >= buttonsHoldDuration) {
+      exitSubMenu();
+    }
+  } else if (digitalRead(downButton) == HIGH || digitalRead(enterButton) == HIGH) {
+    // Reset the button press time if any button is released before hold time
+    buttonPressTime = 0;
+  }
+
+  // Down button handling (Move through menu)
   if (digitalRead(downButton) == LOW && currentMillis - lastButtonPressTime > debounceDelay) {
-    if (inSubMenu) {
-      // If in submenu, check if held for 2 seconds
-      if (downButtonPressTime == 0) {
-        downButtonPressTime = currentMillis; // Start tracking the hold time
-      } else if (currentMillis - downButtonPressTime >= downButtonHoldDuration) {
-        // Exit submenu after holding for 2 seconds
-        exitSubMenu();
-      }
-    } else {
+    if (!inSubMenu) {
       selectedItem = (selectedItem + 1) % menuLength;
       drawMenu();
     }
     lastButtonPressTime = currentMillis; // Update the last button press time
-  } else if (digitalRead(downButton) == HIGH && downButtonPressTime > 0) {
-    // Reset the down button press time if button is released before hold time
-    downButtonPressTime = 0;
+  }
+
+  // Speed adjustment in Speed submenu
+  if (inSubMenu && selectedItem == 3) {  // When we are in the "Speed" submenu
+
+    
+    if (digitalRead(downButton) == HIGH && digitalRead(enterButton) == LOW) {
+      // Down button is pressed, increase speed by 2
+      speed = min(255, speed + 1); // Increase speed by 2 (make sure it doesn't exceed 255)
+      updateSpeedDisplay();
+      delay(200); // Give a small delay to avoid multiple triggers due to bouncing
+    }
+
+    if (digitalRead(enterButton) == HIGH && digitalRead(downButton) == LOW) {
+      // Enter button is pressed, decrease speed by 2
+      speed = max(0, speed - 1); // Decrease speed by 2 (make sure it doesn't go negative)
+      updateSpeedDisplay();
+      delay(200); // Give a small delay to avoid multiple triggers due to bouncing
+    }
   }
 }
 
@@ -113,13 +141,24 @@ void enterSubMenu() {
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.drawString("Entering: ", 10, 20, 4);
   tft.drawString(menu[selectedItem], 10, 50, 4);
+  
+  if (selectedItem == 3) { // If we are in the Speed submenu
+    updateSpeedDisplay();
+  }
 }
 
 void exitSubMenu() {
   inSubMenu = false; // Set the flag to false indicating we are exiting the submenu
   EEPROM.write(eepromStateAddress, 0); // Store the state as main menu (0) in EEPROM
+  EEPROM.write(eepromSpeedAddress, speed); // Save the speed value in EEPROM
   EEPROM.commit(); // Commit the change to EEPROM
   drawMenu(); // Return to the main menu
   delay(500); // Delay to make the transition smoother
-  downButtonPressTime = 0; // Reset the down button hold time
+  buttonPressTime = 0; // Reset the button hold time
+}
+
+void updateSpeedDisplay() {
+  tft.fillRect(10, 100, 200, 30, TFT_BLACK); // Clear the previous speed display
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.drawString("Speed: " + String(speed), 10, 100, 4); // Display the new speed
 }
